50adc7df44c0f782806fbbe5724935ac55a6f9fe
KartikTakyar0046 <42770126+KartikTakyar0046@users.noreply.github.com>
2021-10-08 13:25:55 +0530

    Update bfs.py ewith explanation

--- bfs.py
+++ bfs.py
@@ -1,33 +1,66 @@
-graph = {
+# Python3 Program to print BFS traversal
+# from a given source vertex. BFS(int s)
+# traverses vertices reachable from s.
+from collections import defaultdict
 
-    'A': ['B', 'C'],
-    'B': ['A', 'D', 'F'],
-    'C': ['A', 'E'],
-    'D': ['B', 'I'],
-    'E': ['C', 'F', 'G'],
-    'F': ['B', 'E', 'G'],
-    'G': ['F', 'K'],
-    'I': ['K'],
-    'K': ['I', 'G']
-}
+# This class represents a directed graph
+# using adjacency list representation
+class Graph:
 
-visited = []
-queue = []
+	# Constructor
+	def __init__(self):
 
+		# default dictionary to store graph
+		self.graph = defaultdict(list)
 
-def bfs(graph, visited, node):
-    visited.append(node)
-    queue.append(node)
+	# function to add an edge to graph
+	def addEdge(self,u,v):
+		self.graph[u].append(v)
 
-    while queue:
-       # print(queue)
-        s = queue.pop(0)
-        print(s)
+	# Function to print a BFS of graph
+	def BFS(self, s):
 
-        for neighbour in graph[s]:
-            if neighbour not in visited:
-                visited.append(neighbour)
-                queue.append(neighbour)
+		# Mark all the vertices as not visited
+		visited = [False] * (max(self.graph) + 1)
 
+		# Create a queue for BFS
+		queue = []
 
-bfs(graph, visited, 'A')
+		# Mark the source node as
+		# visited and enqueue it
+		queue.append(s)
+		visited[s] = True
+
+		while queue:
+
+			# Dequeue a vertex from
+			# queue and print it
+			s = queue.pop(0)
+			print (s, end = " ")
+
+			# Get all adjacent vertices of the
+			# dequeued vertex s. If a adjacent
+			# has not been visited, then mark it
+			# visited and enqueue it
+			for i in self.graph[s]:
+				if visited[i] == False:
+					queue.append(i)
+					visited[i] = True
+
+# Driver code
+
+# Create a graph given in
+# the above diagram
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print ("Following is Breadth First Traversal"
+				" (starting from vertex 2)")
+g.BFS(2)
+
+# This code is contributed by Neelam Yadav
